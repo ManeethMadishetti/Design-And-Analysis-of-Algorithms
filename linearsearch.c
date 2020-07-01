71308517614b5bd1aebc5c91259081efd8706b78
SujithaTadi <60035243+sujithatadi@users.noreply.github.com>
2020-07-01 14:19:14 +0530

    Add files via upload

---
+++ linearsearch.c
@@ -0,0 +1,29 @@
+#include <stdio.h> 
+  
+int search(int arr[], int n, int x) 
+{ 
+    int i; 
+    for (i = 0; i < n; i++) 
+        if (arr[i] == x) 
+            return i; 
+    return -1; 
+} 
+  
+int main(void) 
+{ 
+int n,x,i;
+    printf("enter the number of elements:");
+	scanf("%d",&n);
+	int arr[n];
+	for(i=0;i<n;i++){
+		scanf("%d",&arr[i]);
+	}
+    printf("enter the number to search:");
+	scanf("%d",&x);
+   
+    int result = search(arr, n, x); 
+    (result == -1) ? printf("Element is not present in array") 
+                   : printf("Element is present at index %d", 
+                            result); 
+    return 0; 
+} 
